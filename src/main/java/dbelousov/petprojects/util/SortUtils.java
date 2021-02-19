a9f8925a2f1827c5b1a300b08d9b8c5c786ca3d9
Dmitriy Belousov <dmitriy.belousov@barometerit.com>
2021-02-19 16:03:09 -0600

    Implemented merge sort

--- src/main/java/dbelousov/petprojects/util/SortUtils.java
+++ src/main/java/dbelousov/petprojects/util/SortUtils.java
@@ -1,6 +1,13 @@
 package dbelousov.petprojects.util;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class SortUtils {
+
+    // region quicksort
 
     public static <T extends Comparable<T>> void quickSort(T[] in) {
         quickSortInternal(in, 0, in.length - 1);
@@ -37,4 +44,104 @@ public class SortUtils {
         in[left] = in[right];
         in[right] = tmp;
     }
+
+    //endregion quicksort
+
+    // region mergesort
+
+    private static int[] merge(int[] left, int[] right) {
+        var maxIter = left.length + right.length;
+        var merged = new int[maxIter];
+        var mergedCurrIndex = 0;
+        var leftCurrIndex = 0;
+        var rightCurrIndex = 0;
+
+        for (var i = 0; i < maxIter; i++) {
+            if (leftCurrIndex < left.length) {
+                if (rightCurrIndex < right.length) {
+                    if (left[leftCurrIndex] > right[rightCurrIndex]) {
+                        merged[mergedCurrIndex] = right[rightCurrIndex];
+                        rightCurrIndex++;
+                    } else {
+                        merged[mergedCurrIndex] = left[leftCurrIndex];
+                        leftCurrIndex++;
+                    }
+                    mergedCurrIndex++;
+                }
+            }
+        }
+
+        while (leftCurrIndex < left.length) {
+            merged[mergedCurrIndex] = left[leftCurrIndex];
+            mergedCurrIndex++;
+            leftCurrIndex++;
+        }
+
+        while (rightCurrIndex < right.length) {
+            merged[mergedCurrIndex] = right[rightCurrIndex];
+            mergedCurrIndex++;
+            rightCurrIndex++;
+        }
+
+        return merged;
+    }
+
+    private static void swap(int[] one, int[] two, int i) {
+        var tmp = one[i];
+        one[i] = two[i];
+        two[i] = tmp;
+    }
+
+    private static void swap(int[] one, int left, int right) {
+        var tmp = one[left];
+        one[left] = one[right];
+        one[right] = tmp;
+    }
+
+    public static int[] mergeSort(int[] in) {
+        if (in != null) {
+            if (in.length == 1) {
+                return in;
+            }
+            return mergeSortInternal(in);
+        }
+        return null;
+    }
+
+    private static int[] mergeSortInternal(int[] in) {
+        if (in.length == 1) {
+            return in;
+        }
+
+        int mid = in.length / 2;
+        int[] left = getLeftPart(in, mid);
+        int[] right = getRightPart(in, mid);
+        left = mergeSortInternal(left);
+        right = mergeSortInternal(right);
+        return merge(left, right);
+    }
+
+    private static int[] getLeftPart(int[] in, int mid) {
+        var ret = new int[mid];
+
+        for (var i = 0; i < mid; i++) {
+            ret[i] = in[i];
+        }
+
+        return ret;
+    }
+
+    private static int[] getRightPart(int[] in, int mid) {
+        var ret = new int[in.length - mid];
+        var retCurrIndex = 0;
+
+        for (var i = mid; i < in.length; i++) {
+            ret[retCurrIndex] = in[i];
+            retCurrIndex++;
+        }
+
+        return ret;
+    }
+
+    //endregion mergesort
 }
--- src/test/java/dbelousov/petprojects/util/SortUtilsTest.java
+++ src/test/java/dbelousov/petprojects/util/SortUtilsTest.java
@@ -14,4 +14,11 @@ class SortUtilsTest {
         SortUtils.quickSort(in);
         assertEquals("[-76, 1, 3, 4, 5, 5, 5, 6, 7, 17, 85]", Arrays.toString(in));
     }
+
+    @Test
+    void testMergeSort() {
+        var in = new int[] { 7, 5, 1, 5, 4, 6, 3, 5, 17, 85, -76 };
+        int[] sorted = SortUtils.mergeSort(in);
+        assertEquals("[-76, 1, 3, 4, 5, 5, 5, 6, 7, 17, 85]", Arrays.toString(sorted));
+    }
 }
